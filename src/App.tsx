40c11cd45312d9da8c5119612c0fb3e8e06a87d0
Huxley <huxley@Huxleys-MacBook-Pro.local>
2022-09-24 15:21:52 +0200

    my problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./App.css";
-import { Accordian, ColourFulPage, BoringPage } from "./components/accordian";
+import { Accordian } from "./components/accordian";
 
 function App() {
   return (
     <div className="App">
-      <ColourFulPage colour="blue" />
       {/** As the accordian is marked as type fun, i therefore want only colourful pages to be used */}
       <Accordian type="fun">
-        <ColourFulPage colour="blue" />
+        <Accordian.ColourfulPage colour="blue" />
         {/** i want boring page to be like noooooo */}
-        <BoringPage />
+        <Accordian.BoringPage />
+        {/** P tag should not be allowed */}
+        <p>haha i shouldnt be here</p>
       </Accordian>
     </div>
   );
--- src/components/accordian.tsx
+++ src/components/accordian.tsx
@@ -1,15 +1,26 @@
 import React, { ReactElement } from "react";
 
-export const ColourFulPage: React.FC<{ colour: string }> = ({ colour }) => (
+export const ColourfulPage: React.FC<{ colour: string }> = ({ colour }) => (
   <div> i am {colour}</div>
 );
 
 export const BoringPage: React.FC = () => <div> i am boring page </div>;
 
+/**  typeguard */
+enum validAccordianChildren {
+  ColourfulPage,
+  BoringPage,
+}
+const isAccordianElement = (
+  element: string
+): element is keyof AccordianComposition => {
+  return `${element}` in validAccordianChildren;
+};
+
 type FunAccordian = {
   type: "fun";
-  children: React.ReactNode;
-  //   children: typeof ColourFulPage | typeof ColourFulPage[];
+  //   children: React.ReactNode;
+  children: typeof ColourfulPage | typeof ColourfulPage[];
 };
 type BlandAccordian = {
   type: "bland";
@@ -18,10 +29,39 @@ type BlandAccordian = {
 };
 type AccordianType = FunAccordian | BlandAccordian;
 
-export const Accordian: React.FC<AccordianType> = ({ children }) => {
+type AccordianComposition = {
+  ColourfulPage: typeof ColourfulPage;
+  BoringPage: typeof BoringPage;
+};
+
+interface ChildWithTypeName {
+  type: {
+    name: string;
+  };
+}
+
+const Accordian: AccordianComposition & React.FC<AccordianType> = ({
+  children,
+}) => {
+  /** here i can detect this manually however i want ts giving me errors */
+  React.Children.map(children as any, (child) => {
+    console.log({ child });
+    const typeName = (child as ChildWithTypeName)?.type?.name;
+    if (!isAccordianElement(typeName)) {
+      console.log("This is invalid", typeName);
+    }
+  });
+
+  // props has bland
+
   return (
     <div>
       <>{children}</>
     </div>
   );
 };
+
+Accordian.ColourfulPage = ColourfulPage;
+Accordian.BoringPage = BoringPage;
+
+export { Accordian };
